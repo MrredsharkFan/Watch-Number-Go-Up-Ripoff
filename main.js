4eb30a127c51ca4aeca6aa5ceff7c723fe05b487
MrRedSharkFan <haychiu2021@gmail.com>
2026-03-10 00:10:17 +0800

    pushed endgame to e2000

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 color_loop = ["#fcc","#ebd","#dae","#cbf","#bcf","#adf","#9ef","#aee","#bed","#cec","#ddb","#edb","#fdc"]
-rarity_loop = ["#aaa","#ccc","#eee","#9f9","#afa","#cfc","#9bf","#bdf","#dff","#a9f","#caf","#ecf","#e95","#fb7","#fd9","#f99","#fbb","#fdd"]
+rarity_loop = ["#aaa","#ccc","#eee","#9f9","#afa","#cfc","#9bf","#bdf","#dff","#a9f","#caf","#ecf","#e95","#fb7","#fd9","#f99","#fbb","#fdd","#f7f","#faf","#fcf","#ff7","#ffa","#ffd","#77f","#aaf","#ddf","#f7a","#fac","#fdf","#4bc","#7de","#9ef"]
 
 
 function cl(x) {
@@ -10,23 +10,30 @@ function initPlayer() {
     return {
         points: new Decimal(0),
         upg: new Decimal(0),
-        rp: new Decimal(0)
+        rp: new Decimal(0),
+        rup_spent: new Decimal(0),
+        rup: {"1_1":new Decimal(0)},
     }
 }
 
 player = (typeof (localStorage.getItem("wngu-r")) != null ? JSON.parse(localStorage.getItem("wngu-r")) : initPlayer())
 for (i in player) {
-    player[i] = new Decimal(player[i])
+    if (i != "rup") { player[i] = new Decimal(player[i]) }
+}
+for (i in player.rup) {
+    player.rup[i] = new Decimal(player.rup[i])
 }
 
 function hardReset() {
     player = initPlayer()
+    rupButtonGen()
 }
 
 function getPPS() {
     var p = new Decimal(1)
     p = p.times(upgEffect(player.upg)[1])
     p = p.times(rpBoost())
+    if (typeof(player.rup["1_1"])!="undefined"){p = p.times(player.rup["1_1"].add(1))}
     return p
 }
 
@@ -64,7 +71,7 @@ function barText(x) {
 function buyUpg() {
     if (player.points.gte(upgCost(player.upg))) {
         player.points = player.points.sub(upgCost(player.upg))
-        if (buyMax().sub(player.upg).gte(15)) { player.upg = buyMax().sub(10) }
+        if (buyMax().sub(player.upg).gte(15)) { player.upg = buyMax().sub(14) }
         else { player.upg = player.upg.add(1) }
     }
 }
@@ -82,6 +89,12 @@ function rpText() {
     }
 }
 
+function respec() {
+    player.rup_spent = new Decimal(0)
+    player.rup = {"1_1": new Decimal(0)}
+    document.getElementById("rUpg").innerHTML = rupButtonGen()
+}
+
 function rpBoost() {
     var p = player.rp.add(1).times(10).sub(9).pow(0.65)
     if (p.gte(1e15)) { p = new Decimal(10).pow(p.div(1e15).add(1).log10().pow(0.96)).times(1e15) }
@@ -96,12 +109,110 @@ function rebirth() {
     }
 }
 
+function rupp() {
+    var i = player.rp.add(1).log2().sub(70).max(0)
+    return [i.floor(),new Decimal(2).pow(i.floor().add(71))]
+}
+
+function RUpgCost(u) {
+    var p = u.split("_")
+    return new Decimal((p[0]-p[1])+1).pow(0.95).times(new Decimal(2).pow(p[0])).div(5).max(1).floor()
+}
+
+function buyRUpg(u) {
+    console.log(player.rup[u])
+    if (document.getElementById(u).innerHTML != "") {
+        var u = u.split("_")
+        var p = u.join("_")
+        console.log(u, p)
+        if (player.rup_spent.add(RUpgCost(p)).lt(rupp()[0])&&player.rup[p].eq(0)) {
+            player.rup_spent = player.rup_spent.add(RUpgCost(p))
+            player.rup[p] = new Decimal(1)
+            if (u[1] == 1) {
+                player.rup[[u[0] / 2 * 2 + 1, 1].join("_")] = new Decimal(0)
+            }
+            player.rup[[u[0] / 2 * 2 + 1, u[1] / 2 * 2 + 1].join("_")] = new Decimal(0)
+            document.getElementById("rUpg").innerHTML = rupButtonGen()
+        }
+        
+    }
+}
+
+function rupButtonGen() {
+    var t = "<button onclick=\"respec()\">Respec</button>"
+    for (k in player.rup) {
+        var i = k.split("_")[0]*2/2
+        var j = k.split("_")[1] * 2 / 2-1
+        console.log(i,j)
+        t = t + `<button style="right: ${(i - j - 1) * 10}%; top: ${i * 10}%; background-color: ${cl(j - i + 100)}" id="${i}_${j + 1}" onclick="buyRUpg(${i}+'_'+${j+1})"></button>`
+    }
+        return t
+}
+
+function generateUpgradeText(id) {
+    if (typeof (player.rup[id]) == "undefined") {
+        var s = new Decimal(0) 
+    }
+    else {
+        var s = player.rup[id]
+    }
+    if (id.split("_")[1] == 1) {
+        var t = id.split("_")[0]==1?`Point gain x${format(s.add(1))}.`:`Generators in this row x${format(s.add(1))}`
+    } else {
+        var t = `Boost the generator above by ${format(s)}/s.`
+    }
+    document.getElementById(id).innerHTML = `${t}<br>Cost: ${format(RUpgCost(id))}`
+}
+
+function updateUpg() {
+    for (i in player.rup) {
+        generateUpgradeText(i)
+    }
+    generateUpgradeText("1_1")
+}
+
+function rUpgTick(dt) {
+    for (i in player.rup) {
+        var k = i.split("_")
+        if (typeof (player.rup[k[0] + "_1"]) == "undefined"||i=="1_1") { var mult = new Decimal(1) } else { var mult = player.rup[k[0] + "_1"].add(1) }
+        if (k[1] != 1 && k[0] != 1) {
+            player.rup[k[0] - 1 + "_" + (k[1]-1)] = player.rup[k[0]-1+"_"+(k[1]-1)].add(player.rup[i].times(dt).times(mult))
+        }
+    }
+}
+
+function rarityBar() {
+    threshold = ["10", "100", "1000", "1000000", "1e9", "1e12", "1e25", "1e50", "1e100", "1e150", "1e200", "1e300", "1e500", "1e750", "1e1000", "1e1500", "1e2000", "1e3000", "1e5000", "1e10000", "1e20000", "1e50000", "1e100000","ee6","ee7","ee10","ee12","ee15"]
+    var i = 0
+    while (true) {
+        if (player.points.gte(threshold[i])) {
+            i++
+        } else {break}
+    }
+    var i1 = new Decimal(threshold[i]).add(1).slog()
+    var i2 = new Decimal(threshold[i - 1]).add(1).slog()
+    var i3 = player.points.add(1).slog()
+    return [i,(i3-i2)/(i1-i2)]
+}
+
+function rarityThing() {
+    document.getElementById("rar_text").style.width = rarityBar()[1] * 100 + "%"
+    document.getElementById("rar_text").style["background-color"] = rarity_loop[rarityBar()[0]]
+    document.getElementById("rar_text").innerHTML = `Tier ${rarityBar()[0]} (${(rarityBar()[1]*100).toFixed(2)}% to next [${threshold[rarityBar()[0]]}])`
+}
+
+document.getElementById("rUpg").innerHTML = rupButtonGen()
+
 function update(dt) {
-    document.getElementById("point_display").innerHTML = `<b style="font-size: 25px">${format(player.points)}</b> points<br>${format(getPPS())}/s`
+    document.getElementById("point_display").innerHTML = `<b style="font-size: 25px">${format(player.points)}</b> points (${format(getPPS())}/s)`
     document.getElementById("upg").innerHTML = `<b>Boost your point gain!</b><br>Cost: $${format(upgCost(player.upg))}<br><br>Bought: ${format(player.upg)}`
     document.getElementById("bars").innerHTML = barText(player.upg)
     document.getElementById("reb").innerHTML = rpText()
     document.getElementById("reb_text").innerHTML = `${format(player.rp)} Rebirth points<hr>x${format(rpBoost())} Points`
+    document.getElementById("rup_text").innerHTML = `${format(rupp()[0].sub(player.rup_spent))} / ${format(rupp()[0])}, next at ${format(rupp()[1])} RP`
+    updateUpg()
+    rarityThing()
+    rUpgTick(dt)
     player.points = player.points.add(getPPS().times(dt))
 }
 
